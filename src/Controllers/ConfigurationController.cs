93aa0f8b8c0fc24842f1bf759f6584bf37c881bf
Luis Alexandre Rodrigues <luis.rodrigues@aarin.com.br>
2021-03-22 16:32:53 -0300

    Initial setup.

---
+++ src/Controllers/ConfigurationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace AppConfigurationSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConfigurationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigurationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var keyOne = _configuration.GetValue<string>("primeirakey");
+            var keyTwo = _configuration.GetValue<string>("segundakey");
+
+            return Ok(new { KeyOne = keyOne, KeyTwo = keyTwo });
+        }
+    }
+}
